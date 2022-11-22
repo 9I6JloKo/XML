2788b584cdffb66e88de11c094657dc530132ea4
pupil <ananas20049@gmail.com>
2022-11-22 15:03:25 +0200

    finally

--- Books_NodeJS/index.js
+++ Books_NodeJS/index.js
@@ -16,32 +16,32 @@ Books.belongsToMany(Author, {through: 'authorsBooks'})
 
 async function dataCreate(){
     await db.sync({force:true}); // force - создать или перезаписать базу с нуля
-    dbAdd(data);
+    await dbAdd(data);
+
+    const express = require("express")
+    const cors = require("cors")
+    const app = express()
+
+    app.use(cors())
+
+    app.use(express.json())
+
+    app.use(express.urlencoded({ extended: true}))
+
+    app.get("/", (req,res) => {
+        res.json({ message: "Welcome to library Restful API"})
+    })
+
+    require("./routes/categoryRoutes")(app)
+    require("./routes/authorRoutes")(app)
+    require("./routes/bookRoutes")(app)
+    require("./routes/authorBookRoutes")(app)
+    require("./routes/bookCategoryRoutes")(app)
+
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, () => {
+        console.log(`------------------------------------------------Server is running on port ${PORT}.`)
+    })
 
 }
 dataCreate();
-
-const express = require("express")
-const cors = require("cors")
-const app = express()
-
-app.use(cors())
-
-app.use(express.json())
-
-app.use(express.urlencoded({ extended: true}))
-
-app.get("/", (req,res) => {
-    res.json({ message: "Welcome to library Restful API"})
-})
-
-require("./routes/categoryRoutes")(app)
-require("./routes/authorRoutes")(app)
-require("./routes/bookRoutes")(app)
-require("./routes/authorBookRoutes")(app)
-require("./routes/bookCategoryRoutes")(app)
-
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
-})
--- Books_NodeJS/queries.js
+++ Books_NodeJS/queries.js
@@ -1,37 +1,37 @@
-const db = require('./connect/database.js')
-const {Sequelize,DataTypes,Model} = require('sequelize')
-let Author = require('./models/author.js')
-const { sequelize } = require('./models/books.js')
-let Books = require('./models/books.js')
-let Categories = require('./models/category.js')
-let BooksAuthors = require('./models/authorsbooks.js')
-let BooksCategories = require('./models/bookscategories.js')
+// const db = require('./connect/database.js')
+// const {Sequelize,DataTypes,Model} = require('sequelize')
+// let Author = require('./models/author.js')
+// const { sequelize } = require('./models/books.js')
+// let Books = require('./models/books.js')
+// let Categories = require('./models/category.js')
+// let BooksAuthors = require('./models/authorsbooks.js')
+// let BooksCategories = require('./models/bookscategories.js')
 
-const express = require("express")
-const cors = require("cors")
-Books.belongsToMany(Categories, {through: 'booksCategories'})
-Categories.belongsToMany(Books, {through: 'booksCategories'})
-Author.belongsToMany(Books, {through: 'authorsBooks'})
-Books.belongsToMany(Author, {through: 'authorsBooks'})
-const app = express()
+// const express = require("express")
+// const cors = require("cors")
+// Books.belongsToMany(Categories, {through: 'booksCategories'})
+// Categories.belongsToMany(Books, {through: 'booksCategories'})
+// Author.belongsToMany(Books, {through: 'authorsBooks'})
+// Books.belongsToMany(Author, {through: 'authorsBooks'})
+// const app = express()
 
-app.use(cors())
+// app.use(cors())
 
-app.use(express.json())
+// app.use(express.json())
 
-app.use(express.urlencoded({ extended: true}))
+// app.use(express.urlencoded({ extended: true}))
 
-app.get("/", (req,res) => {
-    res.json({ message: "Welcome to library Restful API"})
-})
+// app.get("/", (req,res) => {
+//     res.json({ message: "Welcome to library Restful API"})
+// })
 
-require("./routes/categoryRoutes")(app)
-require("./routes/authorRoutes")(app)
-require("./routes/bookRoutes")(app)
-require("./routes/authorBookRoutes")(app)
-require("./routes/bookCategoryRoutes")(app)
+// require("./routes/categoryRoutes")(app)
+// require("./routes/authorRoutes")(app)
+// require("./routes/bookRoutes")(app)
+// require("./routes/authorBookRoutes")(app)
+// require("./routes/bookCategoryRoutes")(app)
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
-})
+// const PORT = process.env.PORT || 3000
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}.`)
+// })
